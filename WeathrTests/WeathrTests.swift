beb779c5a4745eb4b40dbdda5735d5f0d0fbd797
sedat Korkmaz <68056590+sedatkimki@users.noreply.github.com>
2026-01-12 10:27:11 +0300

    Initial Commit

---
+++ WeathrTests/WeathrTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeathrTests.swift
+//  WeathrTests
+//
+//  Created by sedat korkmaz on 12.01.2026.
+//
+
+import Testing
+@testable import Weathr
+
+struct WeathrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
